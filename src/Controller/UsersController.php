85facdffdaac0a2002a42eef896811d0e09a7a0d
user.name <mdioumassi@yahoo.fr>
2020-09-24 18:28:11 +0200

    first commiit

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,16 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Form\EditProfileType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
     /**
-     * @Route("/users", name="users")
+     * @Route("/users", name="users-profil")
      */
     public function index()
     {
         return $this->render('users/index.html.twig');
+    }
+
+    /**
+     * @Route("/users/profil/edit", name="edit-profile-user")
+     * @param Request $request
+     */
+    public function editProfile(Request $request, EntityManagerInterface $manager)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $this->getUser());
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('users-profil');
+        }
+
+        return $this->render('users/editprofile.html.twig', [
+           'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/EditProfileType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class EditProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('civility', ChoiceType::class, [
+                'placeholder' => 'Choisir',
+                'choices'  => [
+                    'Mr' => 'MR',
+                    'Mme' => 'MME',
+                    'Mll' => 'MLLE',
+                ],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir choisir une civilité.',
+                    ]),
+                ]
+            ])
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('address', TextType::class)
+            ->add('postal_code', IntegerType::class)
+            ->add('city', TextType::class)
+            ->add('phone_mobile', IntegerType::class)
+            ->add('address_complement', TextType::class, [
+                'required' => false
+            ])
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
