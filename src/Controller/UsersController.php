0546a700cdbe13fa8b2784d4ab1dfe2fcf23ac93
user.name <mdioumassi@yahoo.fr>
2020-09-25 21:51:02 +0200

    modifier profil

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UsersController extends AbstractController
 {
@@ -39,5 +40,27 @@ class UsersController extends AbstractController
         return $this->render('users/editprofile.html.twig', [
            'form' => $form->createView()
         ]);
+    }
+
+    /**
+     *
+     * @Route("/users/password/edti", name="users_passwd_edit")
+     */
+    public function editpas(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $manager)
+    {
+        if($request->isMethod('POST')) {
+            $user = $this->getUser();
+
+            //On vérifie si les 2 mots de passe sont identiques
+            if($request->request->get('pass') == $request->request->get('pass2')) {
+                $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('pass')));
+                $manager->flush();
+                $this->addFlash('message', 'Mot de passe mis à jour avec succés');
+                return $this->redirectToRoute('users-profil');
+            } else {
+                $this->addFlash('error', 'Les deux mots de passe  ne sont pas identiques');
+            }
+        }
+        return $this->render('users/editpass.html.twig');
     }
 }
